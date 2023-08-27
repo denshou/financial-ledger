2e72f745ebed3fd7d821e6a7faa5f63647f0ec2d
denshou <132555762+denshou@users.noreply.github.com>
2023-08-28 02:33:47 +0900

    가계부
    +추가할것 : 날짜 기준 정렬, 기간을 정해서 해당 기간 내의 아이템만 보여주기

--- src/App.js
+++ src/App.js
@@ -2,195 +2,79 @@ import logo from "./logo.svg";
 import "./App.css";
 import Dropbox from "./components/Dropbox";
 import { useState } from "react";
-import ItemData from './components/ItemData';
+import ItemData from "./components/ItemData";
+import Form from "./components/Form";
+import Filter from "./components/Filter";
 
 function App() {
   const options = [
-    { label: "식료품", value: "grocery" },
-    { label: "음료", value: "drink" },
-    { label: "기호식품", value: "favoritefood" },
+    { label: "식료품", value: "grocery", className: "first" },
+    { label: "음료", value: "drink", className: "second" },
+    { label: "기호식품", value: "favoritefood", className: "third" },
   ];
-
-  const [objectState, setObjectState] = useState({
-    name: "",
-    price: 0,
-    type: "",
-    date: new Date(),
-    memo: "",
-    repurchase: false,
-  });
 
   const [items, setItems] = useState([
     {
       id: "e1",
       name: "콜라",
-      price: 2000,
+      price: 3000,
       type: "drink",
       date: new Date(2023, 8, 26),
       memo: "코카콜라",
+      repurchase: false,
+    },
+    {
+      id: "e2",
+      name: "사이다",
+      price: 2000,
+      type: "drink",
+      date: new Date(2023, 8, 28),
+      memo: "칠성사이다",
+      repurchase: true,
+    },
+    {
+      id: "e3",
+      name: "사탕",
+      price: 200,
+      type: "favoritefood",
+      date: new Date(2023, 8, 29),
+      memo: "츄파춥스",
       repurchase: true,
     },
   ]);
-
-  const [isChecked, setIsChecked] = useState(false);
-
-  const checkboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const nameChange = (event) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      name: event.target.value,
-    }));
-  };
-  const priceChange = (event) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      price: event.target.value,
-    }));
-  };
-  const dateChange = (event) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      date: event.target.value,
-    }));
-  };
-  const memoChange = (event) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      memo: event.target.value,
-    }));
-  };
-  const radioChange = (event) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      repurchase: event.target.value,
-    }));
-  };
-
-  const getOption = (data) => {
-    setObjectState((prevState) => ({
-      ...prevState,
-      type: data,
-    }));
-  };
-
-  const submitBtnHandler = (event) => {
-    event.preventDefault();
-
+  const getItemData = (data) => {
     setItems([
       {
         id: Math.random().toString(),
-        name: objectState.name,
-        price: objectState.price,
-        type: objectState.type,
-        date: objectState.date,
-        memo: objectState.memo,
-        repurchase: objectState.repurchase,
+        name: data.name,
+        price: data.price,
+        type: data.type,
+        date: data.date,
+        memo: data.memo,
+        repurchase: data.repurchase,
       },
       ...items,
-    ])
+    ]);
+  };
 
-    setObjectState({
-      id:"",
-      name: "",
-      price: 0,
-      type: "",
-      date: new Date(),
-      memo: "",
-      repurchase: false,
-    });
+  const deleteItem = (id) => {
+    const filteredArray = items.filter((item) => item.id !== id);
+    setItems(filteredArray);
   };
 
   return (
     <div className="App">
-      <form className="form" onSubmit={submitBtnHandler}>
-        <div className="form-box">
-          <div className="name">
-            <label htmlFor="name">이름</label>
-            <input
-              type="text"
-              className="input-lg"
-              id="name"
-              value={objectState.name}
-              onChange={nameChange}
-            />
-          </div>
-          <div className="price">
-            <label htmlFor="price">가격</label>
-            <input
-              type="number"
-              className="input-lg"
-              id="price"
-              value={objectState.price}
-              onChange={priceChange}
-            />
-          </div>
-          <div className="type">
-            <label htmlFor="">유형</label>
-            <div className="drop-input">
-              <Dropbox options={options} getOption={getOption} />
-            </div>
-          </div>
-          <div className="date">
-            <label htmlFor="date">구입 날짜</label>
-            <input
-              type="date"
-              id="date"
-              value={objectState.date}
-              onChange={dateChange}
-            />
-          </div>
+      <Form options={options} getItemData={getItemData} />
 
-          <div className="memo">
-            <label>메모</label>
-            <input
-              type="checkbox"
-              id="memo-check"
-              checked={isChecked}
-              onChange={checkboxChange}
-            />
-            <label htmlFor="memo-check">메모 작성</label>
-            <input
-              type="text"
-              disabled={!isChecked}
-              value={objectState.memo}
-              onChange={memoChange}
-            />
-          </div>
-          <div className="repurchase">
-            <label htmlFor="">재구매 의사</label>
-            <input
-              type="radio"
-              name="re"
-              id="re-yes"
-              value="true"
-              onChange={radioChange}
-            />
-            <label htmlFor="re-yes">한다</label>
-            <input
-              type="radio"
-              name="re"
-              id="re-no"
-              value="false"
-              onChange={radioChange}
-            />
-            <label htmlFor="re-no">안한다</label>
-          </div>
-          <button className="submit-btn" type="submit">
-            확인
-          </button>
-        </div>
-      </form>
-
-      <div className="filter">필터</div>
+      <div className="filter-box">
+        <Filter options={options} items={items} deleteItem={deleteItem} />
+      </div>
 
       {/* 드롭박스// 유형 필터 / 정렬 기준 // 시작 기간  끝 기간  
       정렬 기준 : 가격 높은 순, 가격 낮은 순, 최신 순, 오래된 순 */}
-      <div className="result">
-        <ItemData items={items}/>
-      </div>
+      {/* <div className="result">
+        <ItemData items={items} deleteItem={deleteItem} />
+      </div> */}
     </div>
   );
 }
